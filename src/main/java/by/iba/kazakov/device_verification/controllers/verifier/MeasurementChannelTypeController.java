35ca400fa0320d5e9dd3d382080384451b4aaba7
kan22 <ankazakov1.21@gmail.com>
2022-11-13 22:49:34 +0300

    ELSE22

--- src/main/java/by/iba/kazakov/device_verification/controllers/verifier/MeasurementChannelTypeController.java
+++ src/main/java/by/iba/kazakov/device_verification/controllers/verifier/MeasurementChannelTypeController.java
@@ -6,6 +6,7 @@ import by.iba.kazakov.device_verification.models.MeasurementChannelType;
 import by.iba.kazakov.device_verification.models.MeasurementType;
 import by.iba.kazakov.device_verification.models.Methodology;
 import by.iba.kazakov.device_verification.services.serviceInterfaces.DeviceTypeService;
+import by.iba.kazakov.device_verification.services.serviceInterfaces.MeasurementChannelTypeService;
 import by.iba.kazakov.device_verification.services.serviceInterfaces.MeasurementTypeService;
 import by.iba.kazakov.device_verification.services.serviceInterfaces.MethodologyService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,35 +20,31 @@ import java.util.Set;
 @Controller
 public class MeasurementChannelTypeController {
     @Autowired
-    MeasurementTypeService measurementTypeService;
+    MeasurementChannelTypeService measurementChannelTypeService;
 
 
-    @GetMapping({"/verifier/addDeviceType"})
-    public String addDeviceType (Model model) {
-        model.addAttribute("deviceType", new DeviceType());
-        Set<MeasurementType> measurementTypes = measurementTypeService.findAll();
-        model.addAttribute("measurementTypes", measurementTypes );
-        Set<Methodology> methodologies = methodologyService.findAll();
-        model.addAttribute("methodologies", methodologies );
-        return "verifier/addDeviceType";
+    @GetMapping({"/verifier/addMeasurementChannelType"})
+    public String addMeasurementChannelType (Model model) {
+        model.addAttribute("measurementChannel", new MeasurementChannelType());
+        return "verifier/addMeasurementChannelType";
     }
 
-    @PostMapping("/verifier/addDeviceType")
-    public String saveDeviceType (DeviceType deviceType)  {
-        deviceTypeService.save(deviceType);
-        return "verifier/addDeviceTypeSubmit";
+    @PostMapping("/verifier/addMeasurementChannelType")
+    public String saveMeasurementChannelType (MeasurementChannelType measurementChannelType)  {
+        measurementChannelTypeService.save(measurementChannelType);
+        return "verifier/addMeasurementChannelType";
     }
 
-    @GetMapping(value = {"/verifier/addDeviceTypeSubmit"})
-    public String submitDeviceType() {
-        return "verifier/addDeviceTypeSubmit";
+    @GetMapping(value = {"/verifier/addMeasurementChannelTypeSubmit"})
+    public String submitMeasurementChannelType() {
+        return "verifier/addMeasurementChannelTypeSubmit";
     }
 
-    @GetMapping({"/verifier/allDeviceType"})
-    public String deviceTypeList (Model model){
-        Set<DeviceType> deviceTypes = deviceTypeService.findAll();
-        model.addAttribute("deviceTypes", deviceTypes );
-        return "verifier/showAllDeviceType";
+    @GetMapping({"/verifier/showAllMeasurementChannelTypes"})
+    public String measurementChannelTypeList (Model model){
+        Set<MeasurementChannelType> measurementChannelTypes = measurementChannelTypeService.findAll();
+        model.addAttribute("measurementChannelTypes", measurementChannelTypes );
+        return "verifier/showAllMeasurementChannelTypes";
     }
 
 }
