94f97a816150eaf1e0d6f83132b7af0c45547976
kan22 <ankazakov1.21@gmail.com>
2022-11-20 17:13:46 +0300

    home-191122-3

--- src/main/java/by/iba/kazakov/device_verification/controllers/verifier/VerificationProcessController.java
+++ src/main/java/by/iba/kazakov/device_verification/controllers/verifier/VerificationProcessController.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import by.iba.kazakov.device_verification.models.RowCounter;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Set;
 
@@ -41,13 +43,19 @@ public class VerificationProcessController {
         return "verifier/measurementProtocolHead";
     }
 
-
+    //int w;
     @PostMapping("/verifier/measurementProtocolHead")
     public String saveMeasurementProtocolHead (MeasurementProtocolHead measurementProtocolHead)  {
+
         try {
-        measurementProtocolHeadService.save(measurementProtocolHead);
-            //Integer w = measurementProtocolHead.getId();
-            //System.out.println(w);
+              measurementProtocolHeadService.save(measurementProtocolHead);
+            RowCounter rowCounter = new RowCounter();
+            Integer w = measurementProtocolHead.getDeviceInVerificationMeasurementChannelsQuantity();
+            ArrayList<RowCounter> e = rowCounter.setR(w);
+            System.out.println(e);
+
+              //w =measurementProtocolHead.getDeviceInVerificationMeasurementChannelsQuantity();
+
         }catch (Exception e){
             return "verifier/errorInputProtocolNumber";
         }
@@ -58,13 +66,21 @@ public class VerificationProcessController {
     public String deviceInVerificationMeasurementChannels (Model model) {
         model.addAttribute("deviceInVerificationMeasurementChannel",
                 new DeviceInVerificationMeasurementChannel());
-        //MeasurementProtocolHead measurementProtocolHead=measurementProtocolHeadService.findById(25);
-        MeasurementProtocolHead measurementProtocolHead = new MeasurementProtocolHead();
+
+MeasurementProtocolHead measurementProtocolHead = new MeasurementProtocolHead();
+
+        RowCounter rowCounter = new RowCounter();
+        //ArrayList<Integer> e = rowCounter.setR(w);
+        //System.out.println(e);
+        Integer w = measurementProtocolHead.getDeviceInVerificationMeasurementChannelsQuantity();
+        ArrayList<RowCounter> e = rowCounter.setR(w);
+        System.out.println(e);
+
 
         Set<MeasurementChannelType> measurementChannelTypes = measurementChannelTypeService.findAll();
         model.addAttribute("measurementProtocolHead", measurementProtocolHead);
         model.addAttribute("measurementChannelTypes", measurementChannelTypes);
-
+        model.addAttribute("e", e);
 
         return "verifier/deviceInVerificationMeasurementChannel";
     }
